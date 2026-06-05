8ebe4556aafb4748d1e135a44b6cd0c483b3d33b
Holger Krupp <email@holgerkrupp.de>
2026-06-05 08:29:20 +0200

    Initial Commit

---
+++ Scan/ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanApp.swift
+//  Scan
+//
+//  Created by Holger Krupp on 05.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
